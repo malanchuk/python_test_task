643a3443b7a4ea10ad543114bc4c1fde4baf76a7
Anton Malanchuk <anton.malanchuk@gmail.com>
2020-03-21 13:17:08 +0200

    Update django and DRF versions

--- steelkiwi_python_test_task/apps/redirects_analyzer/admin.py
+++ steelkiwi_python_test_task/apps/redirects_analyzer/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Put your admin classes here
+from .models import RedirectInfo
+
+
+@admin.register(RedirectInfo)
+class RedirectInfoAdmin(admin.ModelAdmin):
+    search_fields = ('redirect_domain_name', 'referrer_domain_name',)
+    list_filter = ('browser', 'os', 'created', 'modified', )
+    list_display = ('redirect_full_url', 'referrer_domain_name',
+                    'browser', 'os', 'created')
--- steelkiwi_python_test_task/apps/redirects_analyzer/models.py
+++ steelkiwi_python_test_task/apps/redirects_analyzer/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Put your models here
+from django_extensions.db.models import TimeStampedModel
+
+
+class RedirectInfo(TimeStampedModel):
+    """ 
+    Model to store info of all redirects.
+    """
+    redirect_full_url = models.URLField(max_length=500)
+    redirect_domain_name = models.CharField(max_length=100)
+    redirect_get_paramenters = models.CharField(max_length=500)
+    
+    referrer_full_url = models.URLField(max_length=500)
+    referrer_domain_name = models.CharField(max_length=100)
+    
+    ip_address = models.GenericIPAddressField()
+    browser = models.CharField(max_length=50)
+    os = models.CharField(max_length=50)
+    platform = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.redirect_full_url
