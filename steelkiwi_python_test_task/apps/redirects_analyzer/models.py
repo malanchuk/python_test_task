a3c8dbd11a8d3a600cef776811c0d8f56d7b1af8
Anton Malanchuk <anton.malanchuk@gmail.com>
2020-03-21 14:00:00 +0200

    Create RedirectInfo model and admin

--- steelkiwi_python_test_task/apps/redirects_analyzer/models.py
+++ steelkiwi_python_test_task/apps/redirects_analyzer/models.py
@@ -9,15 +9,14 @@ class RedirectInfo(TimeStampedModel):
     """
     redirect_full_url = models.URLField(max_length=500)
     redirect_domain_name = models.CharField(max_length=100)
-    redirect_get_paramenters = models.CharField(max_length=500)
-    
-    referrer_full_url = models.URLField(max_length=500)
-    referrer_domain_name = models.CharField(max_length=100)
+    redirect_get_paramenters = models.CharField(max_length=500, blank=True)
+    referrer_full_url = models.URLField(max_length=500, blank=True)
+    referrer_domain_name = models.CharField(max_length=100, blank=True)
     
     ip_address = models.GenericIPAddressField()
-    browser = models.CharField(max_length=50)
-    os = models.CharField(max_length=50)
-    platform = models.CharField(max_length=50)
+    browser = models.CharField(max_length=50, blank=True)
+    os = models.CharField(max_length=50, blank=True)
+    platform = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
         return self.redirect_full_url
